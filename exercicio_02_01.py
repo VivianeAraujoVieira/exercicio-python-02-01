5c0bf83898368f68a1d15e2e012a61c38a774aeb
VivianeAraujo <vyviaraujo@hotmail.com.br>
2022-02-22 14:40:56 -0300

    exercicio2.1
    convertendo expressões para python

---
+++ exercicio_02_01.py
@@ -0,0 +1,22 @@
+"""Como Bootcamps - Stone - /código[s]
+Data: 17/02/2022
+Autor: Viviane Araujo Vieira
+Enunciado: Convertendo expressões matematicas para python"""
+
+
+# Recebe do usuario números para calcular um valor total 
+
+valor1 = float(input("Digite o primeiro valor:"))
+
+valor2 = float(input("digite o segundo valor:"))
+
+valor3 = float(input("Digite o terceiro valor:"))
+
+total = (valor1 + valor2)* valor3
+
+print(total)
+
+
+
+
+
